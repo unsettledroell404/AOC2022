fdec2361feb5f4c0aacc5ce533e6a43acf025d41
Budé <r.x.f.bude@tue.nl>
2022-12-06 17:10:42 +0100

    make it nicer

--- puz6_2022/src/main.rs
+++ puz6_2022/src/main.rs
@@ -2,33 +2,32 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-use std::iter::zip;
+
 fn main() {
     //Check if file exists
     if let Ok(lines) = read_lines("./input.txt") {
         for line in lines {
             if let Ok(s) = line {
-              println!("{:}",s);
               let charlist = s.chars();
               //let mut tmp_charlist = String::from("____");  // <---- for first assignment
               let mut tmp_charlist = String::from("______________"); // <------ for second assignment
               let mut index: i32 = 0;
+
+              //move through the characters in this list
               for character in charlist{
                   tmp_charlist.insert(0,character);
                   tmp_charlist.pop();
+
                   //check if all different
-                  //let unique:bool = check_unique(&tmp_charlist);
-                  
+                  let unique:bool = check_unique(&tmp_charlist);            
                   if (unique == true){
-                    println!("{:?}",tmp_charlist);
-                    println!("{:?}",index+1);
+                    println!("The list of characters: {:?}",tmp_charlist);
+                    println!("The index: {:?}",index+1);
                     break;
                   }
                   index+=1;
-                  //println!("{:?}",tmp_charlist);
               }
             }
-
         }
     }
     else{
